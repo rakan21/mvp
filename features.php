1d4976f08229ca83f1a1ab968340fa00526c661d
Cassidy James Blaede <cassidy@system76.com>
2015-04-20 13:55:01 -0600

    Add initial features page

---
+++ features.php
@@ -0,0 +1,102 @@
+<?php
+    include '_templates/sitewide.php';
+    $page['title'] = 'Features of elementary OS';
+    include $template['header'];
+?>
+
+<div class="row">
+    <div class="column half">
+        <h1>Features</h1>
+        <p>elementary OS Freya is the culmination of years of design and development. Here&rsquo;s what you get out of the box with Freya.</p>
+
+        <h2>Apps</h2>
+        <p>All the apps you need for basic tasks, with thousands more available in the Software Center. Each elementary app has been designed by the same team, developed with the same language and guidelines, and consequently works with and looks beautiful alongside the others.</p>
+    </div>
+</div>
+
+<div class="row features">
+    <div class="column half">
+        <h3>Calendar</h3>
+        <p>A basic calendaring app to keep track of dates and events. Supports online sync via CalDAV and Google Calendar so you can keep track of your events on the go.</p>
+        <h3>Camera</h3>
+        <p>Use your computer’s built-in or USB webcam to take photos. The minimal interface shows you the exact shot you’ll get and lets you preview the photos you’ve already taken.</p>
+        <h3>Files</h3>
+        <p>Browse local or remote files. With multiple options like grid, list, or column view, tabbed browsing, and bookmarks, you can view and organize your files how you choose.</p>
+        <h3>Geary Mail</h3>
+        <p>An advanced native email app, Geary supports multiple accounts, background syncing, and notifications. Plus its numbered dock badge shows you the number of new messages right on the dock. Stay connected and up-to-date outside the browser.</p>
+        <h3>Midori</h3>
+        <p>Surf the web with a fast, lightweight, and standards-compliant web browser. Fully native to elementary OS, Midori lets you use HTML5 website and web apps while being lighter on battery life.</p>
+        <h3>Music</h3>
+        <p>Organize and listen to your music. Browse by albums, use lightning-fast search, and build playlists of your favorites. Integration into the system-wide sound menu and multimedia keys means you can pause and skip tracks without missing a beat of your work or play.</p>
+        <h3>Photos</h3>
+        <p>Import, Organize, and Edit photos. Make a slideshow. Share with Facebook or Flickr.</p>
+    </div>
+
+    <div class="column half">
+        <h3>Scratch</h3>
+        <p>An advanced text-editor, Scratch is the perfect tool for developers. Use the project folder sidebar, syntax highlighting, multiple color schemes, and plugins to build apps and websites.</p>
+        <h3>Simple Scan</h3>
+        <p>Scan documents with a connected USB scanner. Simple preview, cropping, and exports mean you spend less time scanning and more time working.</p>
+        <h3>Software Center</h3>
+        <p>Download thousands of apps available for elementary OS.</p>
+        <h3>System Settings</h3>
+        <p>Manage and tweak system-wide settings all in one place.</p>
+        <h3>Terminal</h3>
+        <p>Tap into the power under the hood. Developers and power users can use advanced command line tools with features like multiple tabs, smart copy/paste, and search that get out of the way.</p>
+        <h3>Videos</h3>
+        <p>Watch movies and videos with a minimal interface. The slim, dark frame literally fades away so you can see more of your movie, while the animated tracking preview lets you find the exact spot with fewer interruptions.</p>
+        <h3>And More</h3>
+        <p>Other apps include Archive Manager for zipped archive files, Document Viewer for PDFs, Font Viewer to view and install new fonts, the Screenshot tool, a Calculator, and more.</p>
+    </div>
+</div>
+
+<div class="row">
+    <div class="column half">
+        <h2>Pantheon</h2>
+        <p>The shell around the apps. Pantheon includes the top panel, bottom dock, app launcher, multitasking, notifications, and window management features</p>
+    </div>
+</div>
+
+<div class="row features">
+    <div class="column half">
+        <h3>Panel</h3>
+        <p>The top panel houses the app launcher, clock, and system indicators. It fades into the background and yet is always-present. Hit “Applications” to open the app launcher, click the clock to see a small calendar, or click one of the indicators to see more info and take relevant actions.</p>
+        <h3>App Launcher</h3>
+        <p>See and launch all your installed apps. View them in an alphabetical grid or organized by category, or quickly search by name or keyword. Right-click an app for quick actions and drag your favorite apps to the dock.</p>
+        <h3>Dock</h3>
+        <p>Your favorite and running apps live in the dock at the bottom of your display. Drag to rearrange apps and right-click them for quick actions. When you maximize an app, it ducks out of the way but is always there when you need it.</p>
+    </div>
+
+    <div class="column half">
+        <h3>Multitasking</h3>
+        <p>Quickly switch between open apps with Alt+Tab, or launch into the multitasking view to see a spread of your open apps and workspaces. Drag apps between workspaces to better organize your workflow.</p>
+        <h3>Notifications</h3>
+        <p>Standardized notifications mean apps can pop up relevant info unobtrusively while still letting you act on or dismiss them. Notifications are organized and animated based on their priority, and you can even toggle notifications per-app from the System Settings app. Enter do-not-disturb mode to completely hide and mute notifications when you’re working on something important or presenting.</p>
+        <h3>And More</h3>
+        <p>Smooth animations, side-by-side window snapping, and a plethora of keyboard shortcuts round out the shell to be both beautiful and powerful.</p>
+    </div>
+</div>
+
+<div class="row">
+    <div class="column half">
+        <h2>Design</h2>
+        <p>elementary prides itself in its design; we’ve created an OS that is equal parts useful, usable, and beautiful.</p>
+    </div>
+</div>
+
+<div class="row features">
+    <div class="column half">
+        <h3>System Stylesheet</h3>
+        <p>The elementary stylesheet, or “GTK theme,” has been designed to be neutral, simple, and unobtrusive. It combines subtle gradients, highlights, shadows, and just the right flatness to stay minimal yet iconic. Rather than chase the latest trends, we’ve learned from skeuomorphism, flat design, and shadows to create something more timeless. With a default light gray theme, paper white transient elements, and an optional per-app dark theme, the stylesheet is both diverse and consistent.</p>
+    </div>
+
+    <div class="column half">
+        <h3>Icons</h3>
+        <p>A consistent design set from app icons to the dock, top panel, and app toolbars makes everything fit together perfectly. Each icon is carefully crafted at every size it will be displayed, ensuring crisp pixel-perfect strokes and clear symbols.</p>
+    </div>
+</div>
+
+
+<?php
+    include $template['footer'];
+?>
